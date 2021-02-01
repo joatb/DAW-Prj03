e47d0849cad25f2b8e94fb690a0a90a2073eb775
José Alberto <32104295+joatb@users.noreply.github.com>
2021-02-01 18:51:42 +0100

    feat: vistes

---
+++ resources/views/factures.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Factures</title>
+    <link rel="stylesheet" type="text/css" href="css/factures.css">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	<script defer src="js/factures.js"></script>
+</head>
+<body>
+	<h2>Factures</h2>
+	<div>
+		<table>
+			<thead>
+				<tr>
+					<th>Núm.</th>
+					<th>Data</th>
+					<th class="tla">P</th>
+					<th class="tha">Client</th>
+					<th>B. Imp.</th>
+					<th>IVA</th>
+					<th>TOTAL</th>
+					<th>Acc</th>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
+	</div>
+	<button id="nova">Nova factura</button>
+</body>
+</html>
